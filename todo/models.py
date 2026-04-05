5931ad9d52bc33b3b503f367eaf08b78a8b1b1f1
sufian-sani <abu5suffian@gmail.com>
2026-04-05 15:50:20 +0600

    add active col into to model

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
