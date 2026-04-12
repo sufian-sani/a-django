248550100e532c693400472aae6d59d86956fde7
sufian-sani <abu5suffian@gmail.com>
2026-04-12 20:17:03 +0600

    replace with class base

--- todo/urls.py
+++ todo/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.task_list, name="task_list"),
-    path("create/", views.create_task, name="create_task"),
-    path("toggle/<int:task_id>/", views.toggle_task, name="toggle_task"),
-    path("delete/<int:task_id>/", views.delete_task, name="delete_task"),
+    path("", views.TaskListView.as_view(), name="task_list"),
+    path("create/", views.TaskCreateView.as_view(), name="create_task"),
+    path("toggle/<int:task_id>/", views.TaskToggleView.as_view(), name="toggle_task"),
+    path("delete/<int:task_id>/", views.TaskDeleteView.as_view(), name="delete_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,44 +1,55 @@
-from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.http import require_POST
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView
 
 from .forms import TaskForm
 from .models import Task
 
 
-@login_required
-def task_list(request):
-    tasks = Task.objects.filter(owner=request.user)
-    form = TaskForm()
-    return render(request, "todo/task_list.html", {"tasks": tasks, "form": form})
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "todo/task_list.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TaskForm()
+        return context
 
 
-@login_required
-@permission_required("todo.add_task", raise_exception=True)
-@require_POST
-def create_task(request):
-    form = TaskForm(request.POST)
-    if form.is_valid():
-        task = form.save(commit=False)
-        task.owner = request.user
-        task.save()
-    return redirect("task_list")
+class TaskCreateView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "todo.add_task"
+    raise_exception = True
+
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.owner = request.user
+            task.save()
+        return redirect("task_list")
 
 
-@login_required
-@permission_required("todo.change_task", raise_exception=True)
-@require_POST
-def toggle_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id, owner=request.user)
-    task.completed = not task.completed
-    task.save(update_fields=["completed"])
-    return redirect("task_list")
+class TaskToggleView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "todo.change_task"
+    raise_exception = True
+
+    def post(self, request, task_id, *args, **kwargs):
+        task = get_object_or_404(Task, id=task_id, owner=request.user)
+        task.completed = not task.completed
+        task.save(update_fields=["completed"])
+        return redirect("task_list")
 
 
-@login_required
-@permission_required("todo.delete_task", raise_exception=True)
-@require_POST
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id, owner=request.user)
-    task.delete()
-    return redirect("task_list")
+class TaskDeleteView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "todo.delete_task"
+    raise_exception = True
+
+    def post(self, request, task_id, *args, **kwargs):
+        task = get_object_or_404(Task, id=task_id, owner=request.user)
+        task.delete()
+        return redirect("task_list")
