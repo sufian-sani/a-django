f1e1fad218c8a91283d3f2995bd153080bfec480
sufian-sani <abu5suffian@gmail.com>
2026-04-12 19:59:04 +0600

    initial permission maange through admin

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
-from django.contrib.auth.decorators import permission_required
 
 from .forms import TaskForm
 from .models import Task
@@ -9,22 +8,25 @@ from .models import Task
 
 @login_required
 def task_list(request):
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save(commit=False)
-            task.owner = request.user
-            task.save()
-            return redirect("task_list")
-    else:
-        form = TaskForm()
-
     tasks = Task.objects.filter(owner=request.user)
-    return render(request, "todo/task_list.html", {"form": form, "tasks": tasks})
+    form = TaskForm()
+    return render(request, "todo/task_list.html", {"tasks": tasks, "form": form})
 
 
 @login_required
 @permission_required("todo.add_task", raise_exception=True)
+@require_POST
+def create_task(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.owner = request.user
+        task.save()
+    return redirect("task_list")
+
+
+@login_required
+@permission_required("todo.change_task", raise_exception=True)
 @require_POST
 def toggle_task(request, task_id):
     task = get_object_or_404(Task, id=task_id, owner=request.user)
