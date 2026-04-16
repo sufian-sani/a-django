fca13cebc9a37f98b7e8787ca86dfee25ce6d28d
sufian-sani <abu5suffian@gmail.com>
2026-04-16 18:25:42 +0600

    Add has_object_permission method to NotePermission and TaskPermission classes for object-level permission checks

--- notes/permissions.py
+++ notes/permissions.py
@@ -4,3 +4,6 @@ from users.permissions import AppModelPermissions
 class NotePermission(AppModelPermissions):
     app_label = "notes"
     model_name = "note"
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
--- todo/permissions.py
+++ todo/permissions.py
@@ -4,3 +4,6 @@ from users.permissions import AppModelPermissions
 class TaskPermission(AppModelPermissions):
     app_label = "todo"
     model_name = "task"
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
