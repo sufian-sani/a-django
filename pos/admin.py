91ce4fc3100de2a9ef0408b7649a145771167867
abu-sani-cliqpack <abu.sani@cliqpack.com>
2026-05-23 00:17:56 +0600

    feat: register Product, Order, and OrderItem models in the admin interface

--- pos/admin.py
+++ pos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Product, Order, OrderItem
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order', 'quantity', 'price_at_time_of_order')
